962c018cb0630a43054025ddad196b31f6f5fa1e
YasseenR <yasseenrouni@gmail.com>
2024-08-19 18:00:50 -0400

    Initial Commit

---
+++ MapKitApp/MapKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppApp.swift
+//  MapKitApp
+//
+//  Created by Yasseen Rouni on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
