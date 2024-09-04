0a19fa094bf194fdd8a2d68483ad8dc4bb2c9c4e
YasseenR <yasseenrouni@gmail.com>
2024-09-04 10:16:30 -0400

    Created Map view

--- MapKitApp/ContentView.swift
+++ MapKitApp/ContentView.swift
@@ -5,18 +5,39 @@
 //  Created by Yasseen Rouni on 8/19/24.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
+
 struct ContentView: View {
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapReader { proxy in
+                Map()
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            print(coordinate)
+                        }
+                    }
+                
+            }
         }
-        .padding()
     }
+    
+    
 }
 
 #Preview {
